d5120b06a9f5e73d33109ca614119f0120d5f3ae
Douglas Volcato <douglasvolcato@gmail.com>
2023-03-02 19:47:13 -0300

    fix(domain): add restaurant query parameter #2

--- src/domain/http/routes/admin/product/update-product/updateProduct-response.ts
+++ src/domain/http/routes/admin/product/update-product/updateProduct-response.ts
@@ -1,7 +1,7 @@
 /**
  * type patch
  * with bearer authorization header
- * /admin/product/update-product?id=328nr283r7
+ * /admin/product/update-product?id=328nr283r7&restaurant=8u93u8z3
  */
 
 type UpdateProductAdminResponse = HttpResponse<Product>;
