337ac6c5e60d882be88324311339389935e99ea2
Douglas Volcato <douglasvolcato@gmail.com>
2023-03-02 20:38:29 -0300

    chore(domain): add admin review route types #2

---
+++ src/domain/http/routes/admin/review/get-all-reviews/getAllReviews-response.ts
@@ -0,0 +1,7 @@
+/**
+ * type get
+ * with bearer authorization header
+ * /admin/review/get-all-reviews?restaurant=8u93u8z3
+ */
+
+type GetAllReviewsAdminResponse = HttpResponse<Review[]>;
---
+++ src/domain/http/routes/admin/review/get-one-review/getOneReview-response.ts
@@ -0,0 +1,7 @@
+/**
+ * type get
+ * with bearer authorization header
+ * /admin/review/get-one-review?id=8732y8723ye78?restaurant=8u93u8z3
+ */
+
+type GetOneReviewAdminResponse = HttpResponse<Review>;
