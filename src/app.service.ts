f44cfb2bc9364a52f253b0a7802bd55b3176dd9f
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-03-02 20:40:04 -0300

    docs(swagger): initial config for swagger and status route #4

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! \n Please check http://localhost:3000/api for Swagger docs...';
+    return 'Server is running! \n Please check http://localhost:3000/docs for Swagger docs...';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cardapio-API')
+    .setDescription('Api destinada a aplicação web de um cardápio digital.')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
