06b59a5a7eeab30506ed8125a330e3883bf8bab9
Douglas Volcato <douglasvolcato@gmail.com>
2023-03-04 21:46:33 -0300

    fix(data): fix class name #63

--- src/data/abstract/usecases/user/createUserUsecase-interface.ts
+++ src/data/abstract/usecases/user/createUserUsecase-interface.ts
@@ -1,6 +1,6 @@
 import { CreateUserDto } from 'src/domain/dto/user/createUser-dto';
 import { User } from 'src/domain/entities/user';
 
-export interface CrateUserUseCaseInterface {
+export interface CreateUserUseCaseInterface {
   execute(createUserDto: CreateUserDto): Promise<User>;
 }
