e380f746b274e7c6d2198e35a7e50b74d966d176
Douglas Volcato <douglasvolcato@gmail.com>
2023-03-04 21:46:52 -0300

    fix(data): fix class name #63

--- src/data/abstract/usecases/user/updateUserUseCase-interface.ts
+++ src/data/abstract/usecases/user/updateUserUseCase-interface.ts
@@ -1,6 +1,6 @@
 import { UpdateUserDto } from 'src/domain/dto/user/updateUser-dto';
 import { User } from 'src/domain/entities/user';
 
-export interface GetOneUserUseCaseInterface {
+export interface UpdateUserUseCaseInterface {
   execute(updateUserDto: UpdateUserDto): Promise<User>;
 }
