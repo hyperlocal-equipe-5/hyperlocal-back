7ee0bdb880c1a617b703b695d372657addae0815
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-03-07 19:25:12 -0300

    feat(presentation):  add `getAllProduct-controller.ts` #65

---
+++ src/presentation/controllers/product/getAllProducts-controller.ts
@@ -0,0 +1,24 @@
+import { GetAllProductsUseCaseInterface } from "src/data/abstract/usecases/product/getAllProductsUseCase-interace";
+import { Product } from "src/domain/entities/product";
+import { HttpRequest } from "src/domain/http/httpRequest";
+import { HttpResponse } from "src/domain/http/httpResponse";
+import { GetAllProductsInterface } from "src/presentation/abstract/controllers/product/getAllProductsController-interface";
+import { Response } from "src/utils/http/response";
+
+export class GetAllProductsController implements GetAllProductsInterface{
+  private readonly getAllProductsUseCase: GetAllProductsUseCaseInterface;
+  public constructor(getAllProductsUseCase: GetAllProductsUseCaseInterface){
+    this.getAllProductsUseCase = getAllProductsUseCase
+  }
+
+  public async execute(httpRequest: HttpRequest): Promise<HttpResponse<Product[]>> {
+    try {
+      const restaurantId = httpRequest.restaurant
+      const allProducts =  await this.getAllProductsUseCase.execute(restaurantId)
+
+      return Response.ok(allProducts)
+    } catch (error) {
+      return Response.notFound(error)
+    }
+  }
+}
