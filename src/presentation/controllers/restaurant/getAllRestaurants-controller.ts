4e4f9b06b07aad08c9a9d30463c7789a8d40b61c
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-03-07 17:17:37 -0300

    feat(presentation):  add all restaurant controllers #65

---
+++ src/presentation/controllers/restaurant/getAllRestaurants-controller.ts
@@ -0,0 +1,23 @@
+import { GetAllRestaurantsUseCaseInterface } from "src/data/abstract/usecases/restaurant/getAllRestaurantsUseCase-interface";
+import { Restaurant } from "src/domain/entities/restaurant";
+import { HttpRequest } from "src/domain/http/httpRequest";
+import { HttpResponse } from "src/domain/http/httpResponse";
+import { GetAllRestaurantsInterface } from "src/presentation/abstract/controllers/restaurant/getAllRestaurantsController-interface";
+import { Response } from "src/utils/http/response";
+
+export class GetAllRestaurantsController implements GetAllRestaurantsInterface{
+  private readonly getAllRestaurantsUseCase: GetAllRestaurantsUseCaseInterface;
+  public constructor(getAllRestaurantsUseCase: GetAllRestaurantsUseCaseInterface){
+    this.getAllRestaurantsUseCase
+  }
+
+  public async execute(httpRequest: HttpRequest): Promise<HttpResponse<Restaurant[]>> {
+   try {
+    const allRestaurants = await this.getAllRestaurantsUseCase.execute()
+    
+    return Response.ok(allRestaurants)
+   } catch (error) {
+    return Response.badRequest(error.message)
+   }
+  }
+}
