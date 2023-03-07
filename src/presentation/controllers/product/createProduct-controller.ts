9bf9bbcfcacf4f7332cc58f1bba3bf01883f190e
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-03-07 19:24:37 -0300

    feat(presentation):  add `createProduct-controller.ts` #65

---
+++ src/presentation/controllers/product/createProduct-controller.ts
@@ -0,0 +1,24 @@
+import { CreateProductUseCaseInterface } from "src/data/abstract/usecases/product/createProductUseCase-interface";
+import { Product } from "src/domain/entities/product";
+import { HttpRequest } from "src/domain/http/httpRequest";
+import { HttpResponse } from "src/domain/http/httpResponse";
+import { CreateProductInterface } from "src/presentation/abstract/controllers/product/createProductController-interface";
+import { Response } from "src/utils/http/response";
+
+export class CreateProductController implements CreateProductInterface {
+  private readonly createProductUseCase: CreateProductUseCaseInterface;
+  public constructor(createProductUseCase: CreateProductUseCaseInterface){
+    this.createProductUseCase = createProductUseCase
+  }
+
+  public async execute(httpRequest: HttpRequest): Promise<HttpResponse<Product>> {
+    try {
+      const createProductDto= httpRequest.body
+      const createdProduct =  await this.createProductUseCase.execute(createProductDto)
+      
+      return Response.created(createdProduct)
+    } catch (error) {
+      return Response.badRequest(error.message)
+    }
+  }
+}
