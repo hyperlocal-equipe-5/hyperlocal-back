00b1034e36618a9cdb458991aba4eaa7cdde6539
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-03-07 20:12:32 -0300

    feat(presentation):  add `getOneCategory-controller.ts` #65

---
+++ src/presentation/controllers/category/getOneCategory-controller.ts
@@ -0,0 +1,25 @@
+import { GetOneCategoryUseCaseInterface } from "src/data/abstract/usecases/category/getOneCategoryUseCase-interface";
+import { Category } from "src/domain/entities/category";
+import { HttpRequest } from "src/domain/http/httpRequest";
+import { HttpResponse } from "src/domain/http/httpResponse";
+import { GetOneCategoryInterface } from "src/presentation/abstract/controllers/category/getOneCategoryController-interface";
+import { Response } from "src/utils/http/response";
+
+export class GetOneCategoryController implements GetOneCategoryInterface{
+  private readonly getOneCategoryUseCase: GetOneCategoryUseCaseInterface;
+  public constructor(getOneCategoryUseCase: GetOneCategoryUseCaseInterface){
+    this.getOneCategoryUseCase = getOneCategoryUseCase
+  }
+
+  public async execute(httpRequest: HttpRequest): Promise<HttpResponse<Category>> {
+    try {
+      const categoryId = httpRequest.id;
+      const restaurantId = httpRequest.restaurant
+      const oneCategory = await this.getOneCategoryUseCase.execute(categoryId, restaurantId)
+
+      return Response.ok(oneCategory)
+    } catch (error) {
+      return Response.notFound(error)
+    }
+  }
+}
