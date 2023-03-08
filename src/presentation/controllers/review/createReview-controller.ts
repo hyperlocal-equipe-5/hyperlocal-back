a83e500a08f9ed982efb158d8bbc0c93356de551
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-03-08 11:34:09 -0300

    feat(presentation):  add `createReview-controller.ts` #65

---
+++ src/presentation/controllers/review/createReview-controller.ts
@@ -0,0 +1,25 @@
+import { CreateReviewUseCaseInterface } from "src/data/abstract/usecases/review/createReviewUseCase-interface";
+import { Review } from "src/domain/entities/review";
+import { HttpRequest } from "src/domain/http/httpRequest";
+import { HttpResponse } from "src/domain/http/httpResponse";
+import { CreateReviewInterface } from "src/presentation/abstract/controllers/review/createReviewController-interface";
+import { Response } from "src/utils/http/response";
+
+export class CreateReviewController implements CreateReviewInterface{
+  private readonly createReviewUseCase: CreateReviewUseCaseInterface;
+  public constructor(createReviewUseCase: CreateReviewUseCaseInterface){
+    this.createReviewUseCase = createReviewUseCase
+  }
+
+  public async execute(httpRequest: HttpRequest): Promise<HttpResponse<Review>> {
+    try {
+
+      const createReviewDto = httpRequest.body;
+      const createdReview = await this.createReviewUseCase.execute(createReviewDto);
+
+      return Response.created(createdReview)
+    } catch (error) {
+      return Response.badRequest(error.message)
+    }
+  }
+}
