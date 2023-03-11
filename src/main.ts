5e185be2d3b52e7e896f43f6cf47c7b9fff90ec0
Douglas Volcato <douglasvolcato@gmail.com>
2023-03-11 00:27:54 -0300

    chore(config): implement env variables #69

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3333);
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
