b4d59547963103a84e7956bdcfdfc42ba05abc12
Douglas Volcato <douglasvolcato@gmail.com>
2023-03-27 02:11:58 -0300

    fix(presentation): remove unused validation

--- src/presentation/controllers/order/createOrder-controller.ts
+++ src/presentation/controllers/order/createOrder-controller.ts
@@ -1,11 +1,9 @@
 import { CreateOrderUseCaseInterface } from 'src/data/abstract/usecases/order/createOrderUseCase-interface';
 import { Order } from 'src/domain/entities/order';
-import { User } from 'src/domain/entities/user';
 import { HttpRequest } from 'src/domain/http/httpRequest';
 import { HttpResponse } from 'src/domain/http/httpResponse';
 import { CreateOrderInterface } from 'src/presentation/abstract/controllers/order/createOrderController-interface';
 import { Response } from 'src/utils/http/response';
-import { UserPermissionValidator } from 'src/utils/validators/userPermission-validator';
 
 export class CreateOrderController implements CreateOrderInterface {
   private readonly createOrderUseCase: CreateOrderUseCaseInterface;
@@ -16,13 +14,6 @@ export class CreateOrderController implements CreateOrderInterface {
 
   public async execute(httpRequest: HttpRequest): Promise<HttpResponse<Order>> {
     try {
-      const loggedUser: User = httpRequest.body.loggedUser;
-      if (!UserPermissionValidator.validate(loggedUser, 'createOrders')) {
-        return Response.unauthorized(
-          'This user has no permition to perform this action.',
-        );
-      }
-
       const createOrderDto = httpRequest.body;
       const createdOrder = await this.createOrderUseCase.execute(
         createOrderDto,
